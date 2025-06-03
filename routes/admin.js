624eb17317fe22d890fd09bb35a0d96369ed8e7c
ayushman1210 <ayushmangupta892@gmail.com>
2025-06-03 19:45:01 +0530

    added code

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const admin=require('../Schema/adminSchema')
 const JWT=require('jsonwebtoken');
 const course = require('../Schema/courseschema');
+const adminauth=require('../middleware/adminauth')
 require('dotenv').config();
 
 
@@ -18,7 +19,6 @@ router.post('/signin',async(req,res)=>{
 
 
 router.post('/signup',async (req,res)=>{
-   const 
  const {email,password}=req.body;
    const Admin=await admin.findOne({
       email:email,
@@ -39,26 +39,40 @@ router.post('/signup',async (req,res)=>{
       })
    }
   
-}~)
+})
 
 
-router.post('/course',(req,res)=>{
+router.post('/course',adminauth,async (req,res)=>{
+   const adminId=req.userId;
 const {title,description,price}=req.body;
-const coursedetail=course.create({title,description,price});
+const coursedetail= await course.create({title,description,price, adminId:adminId});
 res.json({
+   message:"course created",
    coursedetail:coursedetail
 })
 })
 
 
-router.put('/course',(req,res)=>{
-const Course=course.find
+router.put('/course',async (req,res)=>{
+   const adminId=req.userId;
+const {title,description,price,courseId}=req.body;
+const update=await course.UpdateOne({_id:courseId, creatorId:adminId},{title:title, description:description, price:price})
+res.json({
+   message:"course created",
+   updatedData:update
+})
 })
 
 
 
-router.get('/course/bulk',(req,res)=>{
-    
+router.get('/course/bulk',async(req,res)=>{
+    const adminId=req.userId;
+    const course=await course.find({
+      creatorId:adminId
+    })
+    res.json({
+      courses:course
+    })
 })
 
 module.exports=router;
